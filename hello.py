0005e47e83f4fad3d7424cb4a1ac269f7d2eebd5
Eliana Feasley <eliana@khanacademy.org>
2013-10-05 13:22:25 -0700

    init
    Test Plan: Navigated to the appropriate url

---
+++ hello.py
@@ -0,0 +1,35 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+class IntensityTracker(object):
+    def __init__(self):
+        self.intensity = 0
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/intensity/set/<int:intensity>')
+def show_post(intensity):
+    intensity = str(intensity)
+    intensity.save('intensity')
+    # show the post with the given id, the id is an integer
+    return 'intensity set'
+
+
+# @app.route('/intensity')
+# def show_post():
+#     i = load('intensity')
+#     # show the post with the given id, the id is an integer
+#     return i
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+if __name__ == '__main__':
+    app.run()
